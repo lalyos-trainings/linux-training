60feda8b94fdac4cab4d6d7f0f15c177185cf574
Kristof Varga <vkris6651@gmail.com>
2023-04-04 21:11:26 +0200

    trying new things

--- ghub_new_kvarga.sh
+++ ghub_new_kvarga.sh
@@ -6,46 +6,43 @@ ghub() {
     : ${path:? required}
     shift
     curl \
-      -H "Authorization: Bearer $GH_TOKEN" \
-      https://api.github.com/${path#/} \
-     "$@"
+        -H "Authorization: Bearer $GH_TOKEN" \
+        https://api.github.com/${path#/} \
+        "$@"
 }
 
 comment-json() {
-  cat <<EOF
-  {
-    "body":"${1}"
-  }
+    cat <<EOF
+    {
+        "body":"${1}"
+    }
 EOF
 }
 
 add-comment() {
-  declare comment_body=$1 issue_number=${2:-89}
-  : ${comment_body:? required}
-
-  json=$(comment-json "${comment_body}")
-  ghub repos/lalyos-trainings/git-wed/issues/${issue_number}/comments \
+    declare comment_body=$1 issue_number=${2:-89}
+    : ${comment_body:? required}
+    json=$(comment-json "${comment_body}")
+    ghub repos/lalyos-trainings/git-wed/issues/${issue_number}/comments \
     -d "${json}"
 }
 
 
-react-json() {
-      cat <<EOF
-  {
-    "content":"${1}"
-  }
-EOF
-}
-
 react() {
     declare issue_number=$1 reaction=$2
     : ${issue_number:? required}
-
-    ghub repos/lalyos-trainings/git-wed/issues/${issue_number}/reactions | jq .[].content
-
-    if [[ -n $reaction ]]; then
-        json=$(react-json "${reaction}")
+    reaction_options=( "+1" "-1" "laugh" "confused" "heart" "hooray" "rocket" "eyes"  )
+    if [[ $reaction == "-a" ]]; then
+        select input in "${reaction_options[@]}"; do
+        if [[ $REPLY == "q" ]]; then
+            break;
+        fi
         ghub repos/lalyos-trainings/git-wed/issues/${issue_number}/reactions \
-        -d "${json}"
+        -d "{\"content\":\"${input}\"}"
+        done
+    elif [[ -n $reaction ]]; then
+        echo "Not a valid switch, use -a for adding."
+    else
+        ghub repos/lalyos-trainings/git-wed/issues/${issue_number}/reactions | jq .[].content
     fi
 }
