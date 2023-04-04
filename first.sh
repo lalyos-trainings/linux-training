f4be3d8b72703add3516621a70bf9e1f7e8e8e9a
McLoad6 <mcload6@yahoo.com>
2023-04-04 17:30:33 +0200

    ua

--- first.sh
+++ first.sh
@@ -1,5 +1,6 @@
 post_comment(){
-  declare title=$1 body=$2;
-  : ${title:? required} ${body:? required};
-  echo $title $body;
+  declare title=$1 body=$2
+  : ${title:? required} ${body:? required}
+  json=$(issue-json "$@")
+  ghub repos/lalyos-trainings/git-wed/issues/comments/1495894990/comments -X POST -d {"body":"test120"}'
 }
