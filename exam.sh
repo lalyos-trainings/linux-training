c9caee49cc3056192d36fd0bcd533029cf510888
valami-laci <valami-laci@desktop.com>
2023-04-04 20:22:43 +0200

    exam feature final +user selection for emoji +multiple choice possibility +the user will be prompted with choices with any arguments +commented for clarification

--- exam.sh
+++ exam.sh
@@ -1,7 +1,5 @@
 
-
-
-
+# alap ghub függvény
 ghub() {
     declare path=$1
     : ${path:? required}
@@ -12,6 +10,7 @@ ghub() {
      "$@"
 }
 
+# comment jsonifier
 com-json() {
     cat << EOF
 {
@@ -20,6 +19,7 @@ com-json() {
 EOF
 }
 
+# add-comment
 add-comment() {
     declare body=$1 id=${2:-89}
     : ${body:? required}
@@ -42,32 +42,56 @@ EOF
 
 comment-reaction() {
   declare issue_id=$1 reaction_id=$2 
+  # ha nincs issue megadva, akkor befejeződik a program
   : ${issue_id:? required}
 
-
+  # ha nincs reakció, akkor végrehajtja
   if [[ -z ${reaction_id} ]]; then
-  ghub repos/lalyos-trainings/git-wed/issues/${issue_id}/reactions -s | jq .[].content -r
-  echo "================================="
-  echo "would you like to add a reaction? (yes/no)"
-  read user_choice
   
+  # jelenlegi emojik listázása
+  ghub repos/lalyos-trainings/git-wed/issues/${issue_id}/reactions -s | jq .[].content -r
+  else
+
+  # ha volt alapból emoji, akkor hozzáadja az issuehoz
+  # jsonifier
+  reaction=$(reaction-json "${reaction_id}")
+  echo ${reaction_id} added to issue ${issue_id}
+  ghub repos/lalyos-trainings/git-wed/issues/${issue_id}/reactions -d "${reaction}"
+  fi
+
+  # user selection lehetőség loopolva és elválasztva
+  echo "================================="
+  echo "would you like to add another reaction? (yes/no)"
+  read user_choice
+
+  # ha szeretne hozzáadni emojit a user
     if [[ ${user_choice} == "yes" ]]; then
-    # echo ${user_choice}
+    # echo ${user_choice} tesztelés
+  
+  # választható emojik kilistázása, majd user inputra vár
     select chosen_emoji in +1 -1 laugh confused heart hooray rocket eyes
     do
     echo ${chosen_emoji} was chosen for issue: ${issue_id}
+    # jsont csinál a user válaszból
     chosen_emoji_json=$(reaction-json "${chosen_emoji}")
+    # echo ${chosen_emoji_json} tesztelés
     
-    # echo ${chosen_emoji_json}
+    #létrehozza a reakciót a poszton 
     ghub repos/lalyos-trainings/git-wed/issues/${issue_id}/reactions -d "${chosen_emoji_json}"
+    
     
     echo "would you like to react again? (yes/no)"
     read user_choice_2
 
+    # amíg a user yesszel válaszol, addig hozzáadhat új emojit
+    # ha a user nem szeretne többet hozzáadni, akkor befejeződik a function
       if [[ ${user_choice_2} == "no" ]]; then
       break
       fi
     
+
+    # válaszlehetőséges listázása újra és újabb user inputra vár
+    # nem tudok kitalálni jobb megoldást most egy listára.
     echo "1) +1"
     echo "2) -1"
     echo "3) laugh"
@@ -76,21 +100,11 @@ comment-reaction() {
     echo "6) hooray"
     echo "7) rocket"
     echo "8) eyes"
-    # nem találtam jobb megoldást most egy listára.
     echo "choose a reaction:"
 
     done
 
     fi
-
-
-  else
-  reaction=$(reaction-json "${reaction_id}")
-  echo ${reaction_id} added to issue ${issue_id}
-  ghub repos/lalyos-trainings/git-wed/issues/${issue_id}/reactions -d "${reaction}"
-
-  fi
-
 }
 
 
