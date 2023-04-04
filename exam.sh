41f4e50040cd349b2a151ac57aa398b06872e3d6
laszlob <laszlo.barnabas123@gmail.com>
2023-04-04 17:01:48 +0200

    react started, comment number added

--- exam.sh
+++ exam.sh
@@ -21,9 +21,49 @@ EOF
 }
 
 comment() {
-  declare comment=$1 issuenumber=$2
-  : ${comment:? required} 
+  declare comment=$1 
+  #issuenumber=$2
+  ${issuenumber:='89'}
+  : ${comment:? required}   
   : ${issuenumber:? required} 
   json=$(comment-json "$@")
-  ghub repos/lalyos-trainings/git-wed/issues/"$issuenumber"/comments -d "${json}"
+  
+  echo ghub repos/lalyos-trainings/git-wed/issues/"$issuenumber"/comments -d "${json}"
+
+}
+
+
+
+# issue-json() {
+#   cat <<EOF
+# {
+#   "title":"${1:? title required}",
+#   "body":"${2:? body required}"
+# }
+# EOF
+# }
+
+# issue() {
+#   declare title=$1 body=$2
+#   : ${title:? required} ${body:? required}
+
+#   json=$(issue-json "$@")
+#   ghub repos/lalyos-trainings/git-wed/issues -d "${json}"
+# }
+
+react-json() {
+  cat <<EOF
+{
+  "issuenumber":"${1:? issuenumber required}",
+  "reaction":"${2:? body required}"
+}
+EOF
+}
+
+react() {
+  declare issuenumber=$1 reaction=$2
+  : ${issuenumber:? required} 
+    
+  json=$(issue-json "$@")
+  ghub repos/lalyos-trainings/git-wed/issues -d "${json}"
 }
