9d62b765b819b8d5c13c5c17d00ccdd4124cdccb
tothandras2019 <toth.andras.2000@gmail.com>
2023-04-04 18:20:11 +0200

    modification

--- exmfile.sh
+++ exmfile.sh
@@ -1,7 +1,9 @@
 #old=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
 #new=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
 
-GH_TOKEN=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
+#old=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
+
+GH_TOKEN=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
 
 alias r="source $BASH_SOURCE"
 
@@ -21,6 +23,12 @@ cat <<EOF
 EOF
 }
 
+addReaction(){
+    cat <<EOF
+{"content": "$1"}
+EOF
+}
+
 passIss(){
     declare issNum=${1:-89}
     declare comment=${2:-"no comment here"}
@@ -35,8 +43,13 @@ passIss(){
 # ha már van: +1, -1, új: laugh, confused, heart, hooray, rocket, eyes
 listReactions(){
     declare id=${1:-89}
+    # ha nincs id írja ko hogy kötelező!
 
-    ghub repos/lalyos-trainings/git-wed/issues/${id}/reactions -s | jq .[]
+    #ghub repos/lalyos-trainings/git-wed/issues/${id}/reactions -s | jq .[].content # ki kell listáznia!
+    if [ $2  ];then 
+        contenJson=$(addReaction "$2")
+        #ghub repos/lalyos-trainings/git-wed/issues/${id}/reactions -d "${contenJson}"
+    fi
 
 
 }
