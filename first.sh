a6331430e8f29f3e307d0ff815a467ad67414883
McLoad6 <mcload6@yahoo.com>
2023-04-05 08:05:18 +0200

    linkre meg nem jottem ra

--- first.sh
+++ first.sh
@@ -14,4 +14,23 @@ post_comment(){
   ghub /repos/lalyos-trainings/git-wed/issues/${numb}/comments -d "${json}"
 }
 
-ghub /repos/lalyos-trainings/git-wed/issues/89/comments -d '{"body":"test"}'
+react(){
+  declare issue_number=$1 reaction_text=$2
+  if [[ -z $1 ]]; then
+    echo issue number is missing
+    return
+  fi
+  if [[ $1 == 89 ]]; then
+    if [[ -z $2 ]]; then
+      echo list reactions
+    else 
+      echo add some reaction
+    fi
+  else
+    echo not such an issue
+  fi
+
+}
+
+
+ghub /repos/lalyos-trainings/git-wed/issues/89/reactions -d '{"rocket":"1"}'
